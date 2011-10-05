c8cada256ddb7ffa810e08134ce9f1c78df9d6f4
Adron Hall <adronhall@gmail.com>
2011-10-04 21:20:10 -0700

    Added a css theme, adding some images and basic text around what everything does.

---
+++ AWS_MVC_Web_Applicaiton/Controllers/QuartzController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AWS_MVC_Web_Applicaiton.Controllers
+{
+    public class QuartzController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
