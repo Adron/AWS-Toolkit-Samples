f1ac2fa526c634e074a3bca6437e59667a5ff105
Adron Hall <adronhall@gmail.com>
2011-12-06 22:34:42 -0800

    Removed Class1.cs file.
    Added HomeControllerTests.cs and created the first three tests.
    
    Edit in csproj file made.

---
+++ Web_Application_Unit_Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using AWS_MVC_Web_Applicaiton.Controllers;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Web_Application_Unit_Tests.Controllers
+{
+    [TestFixture]
+    public class when_controller_actions_are_executed
+    {
+        HomeController controller;
+
+        [TestFixtureSetUp]
+        public void SetupController()
+        {
+            controller = new HomeController();
+        }
+
+        [Test]
+        public void should_return_index_view()
+        {
+            controller.Index().ShouldNotBe(null);
+        }
+
+        [Test]
+        public void should_return_contact_view()
+        {
+            controller.Index().ShouldNotBe(null);
+        }
+
+        [Test]
+        public void should_return_links_view()
+        {
+            controller.Index().ShouldNotBe(null);
+        }
+    }
+}
