fdcc733bb5a59cac0c89eac9e53b5211f00cb723
unknown <Administrator@ADRONHALL11FF.(none)>
2011-12-21 16:20:49 -0800

    Initial wrapper for the AWS API.

---
+++ AWS_Web_Project_Template/Abstractions/AmazonEC2.cs
@@ -0,0 +1,27 @@
+﻿namespace AWS_Web_Project_Template.Abstractions
+{
+    public class AwsCentralCommand
+    {
+        private readonly IAmazonS3Wrapper _amazonS3;
+        private readonly IAmazonEc2Wrapper _amazonEc2;
+
+        public AwsCentralCommand(IAmazonEc2Wrapper amazonEc2)
+        {
+            _amazonEc2 = amazonEc2;
+        }
+
+        public AwsCentralCommand(IAmazonS3Wrapper amazonS3)
+        {
+            _amazonS3 = amazonS3;
+        }
+
+        public AwsCentralCommand(IAmazonEc2Wrapper amazonEc2, IAmazonS3Wrapper  amazonS3)
+        {
+            _amazonEc2 = amazonEc2;
+            _amazonS3 = amazonS3;
+        }
+
+        public IAmazonEc2Wrapper Ec2 { get; set; }
+    }
+
+}
